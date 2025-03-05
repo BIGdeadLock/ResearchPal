b5fc3ddd8bb7f96b922aa948e10a0da6eb446f5b
BigDeadLock <eden.yavin25@gmail.com>
2025-03-05 20:19:46 +0200

    etl and fe pipelines are working with arxiv papers

--- pipelines/digital_data_etl.py
+++ pipelines/digital_data_etl.py
@@ -1,11 +1,19 @@
 from zenml import pipeline
 
-from steps.etl import crawl_links, get_or_create_user
+from llm_engineering.domain.queries import PaperQuery
+from steps.etl import crawl_links, fetch_papers, get_or_create_user
 
 
 @pipeline
 def digital_data_etl(user_full_name: str, links: list[str]) -> str:
     user = get_or_create_user(user_full_name)
     last_step = crawl_links(user=user, links=links)
+    return last_step.invocation_id
 
+
+@pipeline
+def papers_digital_data_etl(user_full_name: str, papers: list[dict]) -> str:
+    user_full_name = get_or_create_user(user_full_name)
+    papers = [PaperQuery(source=p["source"], query=p["query"]) for p in papers]
+    last_step = fetch_papers(user=user_full_name, papers_queries=papers)
     return last_step.invocation_id
